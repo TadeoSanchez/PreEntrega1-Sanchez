00bcac20f27e527f786410d4da7ef86649e79bc2
Tadeo Sánchez <tadeosanchez125@gmail.com>
2023-06-12 23:25:08 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function ingresarDatosAlumno() {
+  let nombre = prompt("Ingrese el nombre del alumno");
+  let curso = prompt("Ingrese el curso del alumno");
+  return { nombre, curso };
+}
+
+function calcularNotas(nota1, nota2, nota3) {
+  return (nota1 + nota2 + nota3) / 3;
+}
+
+function aprobadoNoAprobado(promedio) {
+  if (promedio >= 6) {
+    console.log(`El alumno está aprobado`);
+  } else {
+    console.log(`El alumno está desaprobado`);
+  }
+}
+
+function main() {
+  let opcion;
+  do {
+    const alumno = ingresarDatosAlumno();
+    console.log(`Alumno: ${alumno.nombre}`);
+    console.log(`Curso: ${alumno.curso}`);
+
+    let nota1 = parseFloat(prompt("Ingrese la primera nota"));
+    let nota2 = parseFloat(prompt("Ingrese la segunda nota"));
+    let nota3 = parseFloat(prompt("Ingrese la tercera nota"));
+
+    console.log(`Notas ingresadas: ${nota1}, ${nota2}, ${nota3}`);
+
+    let promedio = calcularNotas(nota1, nota2, nota3);
+    console.log(`Promedio Final: ${promedio}`);
+
+    aprobadoNoAprobado(promedio);
+
+    opcion = prompt("Ingresar 'SALIR' para finalizar");
+  } while (opcion !== "SALIR");
+}
+main();
